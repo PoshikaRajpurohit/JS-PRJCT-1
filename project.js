ade15166e3b946a61765542e7f1166794e6b9514
PoshikaRajpurohit <159758700+PoshikaRajpurohit@users.noreply.github.com>
2024-12-06 10:08:10 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,114 @@
+
+
+// #1 Find the Area of Triangle
+    
+// let b=5 ,h=7;
+// area=0.5*b*h;
+// console.log("The Breadth  of Triangle is=",b);
+// console.log("The Height  of Triangle is=",h);
+// console.log("The Area of Triangle=",area);
+
+
+// Expected Output= Bredth=5 Height=7 Area of Triangle= 17.5
+
+
+// #2 Convert cels into farehnit.
+
+// let temp=200;
+// f=(1.8*temp)+32;
+// console.log("The temperature in celsius=",temp);
+// console.log("The temperature in Fahrenheit=",f);
+
+// Expected Output= temp in clesius=200  Cesius into Farehnit= 392
+
+// #3 Findout Area of rectangle
+
+// let length=20 ,width=15;
+// area=length*width;
+// console.log("The length of rectangle=",length);
+// console.log("The width  of rectangle=",width);
+// console.log("The Area of Rectangle=",area);
+
+    // Expected Output= width=15 length=20 Area of Rectangle= 300
+
+// #4 Find Area of Circle 
+    // let radius=10;
+    // area=3.14*radius**2;
+    // console.log("The radius of the circle=",radius);
+    // console.log("The area of the circle=",area);
+
+    
+// Expected Output=radius=10  Area of circle= 314
+
+
+// #5 Convert feet into inches.
+
+    // let inches=12,feet=5;
+    // convert=feet*inches;
+    // console.log("Feets=",feet);
+    // console.log(" Conversion of feet into inches=",convert);
+
+    // Expected Output= feet=5 feet into inches=60
+
+
+   
+   
+    // #6 programs for following formulas.
+
+    // 1) 
+    // let a=20, b=10;
+    // SQofab = (a-b)*(a+b) ;
+    // console.log("The value of A=",a);
+    // console.log("The value of B=",b);
+    // console.log("The square of a**2-b**2 =",SQofab);
+
+     // Expected Output= a=20 b=10  square of a-b= 300 
+
+
+    // #2)
+    // let a=20, b=5;
+    // wholeSQofab = a**2-2*a*b +b**2 ;
+    // console.log("The value of A=",a);
+    // console.log("The value of B=",b);
+    // console.log("The whole square of (a-b) =",wholeSQofab);
+
+    //  // Expected Output= a=20 b=5  whole square of a-b=225 
+
+    // #3)
+    // let a=30 ,b=40, c=10;
+    // wholeSQofabc=a**2+b**2+c**2+2*a*b+2*a*c+2*b*c;
+    // console.log("The value of A=",a);
+    // console.log("The value of B=",b);
+    // console.log("The value of C=",c);
+    // console.log("The whole square of (a+b+c) =",wholeSQofabc);
+
+
+     //  // Expected Output= a=30 b=40 c=10  whole square of a+b+c=6400 
+
+    //  #4)
+    // let a=10, b=20 , c=30;
+    // wholeSQofabc=a**2+b**2+c**2-2*a*b-2*a*c+2*b*c;
+    // console.log("The value of A=",a);
+    // console.log("The value of B=",b);
+    // console.log("The value of C=",c);
+    // console.log("The whole square of (a-b-c) =",wholeSQofabc);
+
+
+
+     //  // Expected Output= a=10 b=20 c=30  whole square of a-b-c=1600 
+
+
+    //  #5)
+    // let a=20, b=10;
+    // wholecubeofab=a**3-3*a**2*b+3*a*b**2-b**3;
+    // console.log("The value of A=",a);
+    // console.log("The value of B=",b);
+    // console.log("The whole cube of (a-b) =",wholecubeofab);
+
+
+
+     //  // Expected Output= a=20 b=10  whole cube of a-b=1000 
+
+
+
+    
